1cabd75eb5cf2b2769754d769cfa04fa2915e4de
Winnie Brandão <winnie_brandao@yahoo.com.br>
2023-03-22 16:24:07 +0000

    feat: add service to createContacts

--- back-end/src/services/contacts/createContacts.service.ts
+++ back-end/src/services/contacts/createContacts.service.ts
@@ -1,5 +1,24 @@
 import { IContactRequest, IContactResponse } from "../../interfaces";
+import appDataSource from "../../data-source";
+import { Contacts } from "../../entities/contacts.entity";
+import { contactResponseSerializer } from "../../serializers/contacts.serializer";
+import { AppError } from "../../errors/AppErrors";
 
 export const createContactsService = async (
   contactsData: IContactRequest
-)/*: Promise<IContactResponse>*/ => {};
+): Promise<IContactResponse> => {
+  const { email } = contactsData;
+  const contactsRepository = appDataSource.getRepository(Contacts);
+
+  const emailExists = await contactsRepository.findOneBy({ email: email });
+
+  if (!emailExists) {
+    const contacts = contactsRepository.create(contactsData);
+    await contactsRepository.save(contacts);
+    const contactsResponse = contactResponseSerializer.parse(contacts);
+
+    return contactsResponse;
+  }
+
+  throw new AppError("Email already exists", 409);
+};
