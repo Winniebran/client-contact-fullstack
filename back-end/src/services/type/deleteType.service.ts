61d7bc2acaf6cb33ab3415d8f990e6d726d0917d
Winnie Brandão <winnie_brandao@yahoo.com.br>
2023-03-22 17:42:24 +0000

    feat: add service to deleteType

--- back-end/src/services/type/deleteType.service.ts
+++ back-end/src/services/type/deleteType.service.ts
@@ -1,1 +1,16 @@
-export const deleteTypeService = async (id: string) => {};
+import appDataSource from "../../data-source";
+import { Type } from "../../entities/type.entity";
+import { AppError } from "../../errors/AppErrors";
+
+export const deleteTypeService = async (id: string) => {
+  const typeRepository = appDataSource.getRepository(Type);
+
+  const foundType = await typeRepository.findOneBy({
+    id: id,
+  });
+  if (!foundType) {
+    throw new AppError("Type not found", 404);
+  }
+
+  await typeRepository.delete(id);
+};
