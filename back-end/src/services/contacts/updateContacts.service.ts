3fda9bbe54bcab0ac523935452c12af2c5807878
Winnie Brandão <winnie_brandao@yahoo.com.br>
2023-03-22 16:49:30 +0000

    feat: add service to updateContacts

--- back-end/src/services/contacts/updateContacts.service.ts
+++ back-end/src/services/contacts/updateContacts.service.ts
@@ -1,6 +1,30 @@
 import { IContactResponse, IContactUpdate } from "../../interfaces";
+import appDataSource from "../../data-source";
+import { Contacts } from "../../entities/contacts.entity";
+import { AppError } from "../../errors/AppErrors";
+import { contactResponseSerializer } from "../../serializers/contacts.serializer";
 
 export const updateContactsService = async (
-    id: string,
-    contactsData: IContactUpdate
-  )/*: Promise<IContactResponse*/ => {};
+  id: string,
+  contactsData: IContactUpdate
+): Promise<IContactResponse> => {
+  const contactsRepository = appDataSource.getRepository(Contacts);
+
+  const [foundContacts] = await contactsRepository.find({
+    where: { id: id },
+  });
+
+  if (!foundContacts) {
+    throw new AppError("Contacts not found", 404);
+  }
+
+  const updatedContacts = contactsRepository.create({
+    ...foundContacts,
+    ...contactsData,
+  });
+  await contactsRepository.save(updatedContacts);
+
+  const updateContacts = contactResponseSerializer.parse(updatedContacts);
+
+  return updateContacts;
+};
