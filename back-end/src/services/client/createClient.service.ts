f1818c4aa60bf80d376cfeb45bb8ab63126ece29
Winnie Brandão <winnie_brandao@yahoo.com.br>
2023-03-22 17:12:06 +0000

    fix: add error message for already registered cell phone

--- back-end/src/services/client/createClient.service.ts
+++ back-end/src/services/client/createClient.service.ts
@@ -7,18 +7,24 @@ import { AppError } from "../../errors/AppErrors";
 export const createClientService = async (
   clientData: IClientRequest
 ): Promise<IClientResponse> => {
-  const { email } = clientData;
+  const { email, cellPhone } = clientData;
   const clientRepository = appDataSource.getRepository(Client);
 
   const emailExists = await clientRepository.findOneBy({ email: email });
-
-  if (!emailExists) {
-    const client = clientRepository.create(clientData);
-    await clientRepository.save(client);
-    const clientWithoutPassword = clientResponseSerializer.parse(client);
-
-    return clientWithoutPassword;
+  if (emailExists) {
+    throw new AppError("Email already registered", 409);
   }
 
-  throw new AppError("Email already exists", 409);
+  const cellPhoneExists = await clientRepository.findOneBy({
+    cellPhone: cellPhone,
+  });
+  if (cellPhoneExists) {
+    throw new AppError("Cell phone already registered", 409);
+  }
+
+  const client = clientRepository.create(clientData);
+  await clientRepository.save(client);
+  const clientWithoutPassword = clientResponseSerializer.parse(client);
+
+  return clientWithoutPassword;
 };
--- back-end/src/services/contacts/createContacts.service.ts
+++ back-end/src/services/contacts/createContacts.service.ts
@@ -7,18 +7,24 @@ import { AppError } from "../../errors/AppErrors";
 export const createContactsService = async (
   contactsData: IContactRequest
 ): Promise<IContactResponse> => {
-  const { email } = contactsData;
+  const { email, cellPhone } = contactsData;
   const contactsRepository = appDataSource.getRepository(Contacts);
 
   const emailExists = await contactsRepository.findOneBy({ email: email });
-
-  if (!emailExists) {
-    const contacts = contactsRepository.create(contactsData);
-    await contactsRepository.save(contacts);
-    const contactsResponse = contactResponseSerializer.parse(contacts);
-
-    return contactsResponse;
+  if (emailExists) {
+    throw new AppError("Email already registered", 409);
   }
 
-  throw new AppError("Email already exists", 409);
+  const cellPhoneExists = await contactsRepository.findOneBy({
+    cellPhone: cellPhone,
+  });
+  if (cellPhoneExists) {
+    throw new AppError("Cell phone already registered", 409);
+  }
+
+  const contacts = contactsRepository.create(contactsData);
+  await contactsRepository.save(contacts);
+  const contactsResponse = contactResponseSerializer.parse(contacts);
+
+  return contactsResponse;
 };
