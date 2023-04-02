512b8686fd144b7161e6ba88145463596d94b85e
Winnie Brandão <winnie_brandao@yahoo.com.br>
2023-04-02 17:23:18 +0100

    feat: add update serializer to client

--- front-end/contacts_book/src/serializers/ClientSerializer.ts
+++ front-end/contacts_book/src/serializers/ClientSerializer.ts
@@ -23,6 +23,26 @@ export const registerClientSerializer = z
   });
 
 export const loginClientSerializer = z.object({
-  email: z.string().trim().nonempty("Email is required").email("Invalid email format"),
+  email: z
+    .string()
+    .trim()
+    .nonempty("Email is required")
+    .email("Invalid email format"),
   password: z.string().trim().nonempty("Password is required"),
+});
+
+export const updateClientSerializer = z.object({
+  firstName: z.string().min(2).trim(),
+  lastName: z.string().min(2).trim(),
+  password: z
+    .string()
+    .trim()
+    .regex(/[A-Z]/, "Must contain a capital letter")
+    .regex(/([a-z])/, "Must contain a lowercase")
+    .regex(/(\d)/, "Must contain a number")
+    .regex(/(\W)|_/, "Must contain a special character")
+    .regex(/.{8,}/, "Must contain at least 8 characters"),
+  email: z.string().trim().email("Invalid email format").min(10),
+  cellPhone: z.string().trim().min(11),
+  image: z.string().trim().nullish(),
 });
